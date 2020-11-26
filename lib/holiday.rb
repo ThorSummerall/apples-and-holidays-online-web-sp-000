f1d7ac24d5ef5d71284b2304ba2f573ad62e8bff
Thor Summerall <thor.summerall@gmail.com>
2020-11-26 17:41:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies = []
+  holiday_hash[:winter][:christmas] << winter_supplies
+  holiday_hash[:winter][:new_years] << winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
