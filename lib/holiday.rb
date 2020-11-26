2af84a9cd114e51e8058f82fad55c3a956290295
Thor Summerall <thor.summerall@gmail.com>
2020-11-26 17:33:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,13 +24,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
   holiday_hash.each do |holi_hash, season|
-    if season == :winter
+    if holi_hash == :winter
       season.each do |attribute, data|
-        puts "#{data}, #{attribute}"
+        data << supply
       end
     end
   end
